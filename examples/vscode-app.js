fcf977ab2fcfdd00eea28ee41c87334668e7fc43
teamchong <25894545+teamchong@users.noreply.github.com>
2026-01-23 13:45:00 -0500

    refactor: simplify vscode-app example

--- examples/vscode-app.js
+++ examples/vscode-app.js
@@ -1,23 +1,8 @@
 #!/usr/bin/env node
 /**
- * VS Code Web - Run vscode.dev in terminal
- *
- * Usage:
- *   node examples/vscode-app.js
- *   node examples/vscode-app.js --no-toolbar
+ * VS Code Web - Run vscode.dev in terminal (no toolbar)
  */
 
 const termweb = require('../lib');
 
-const args = process.argv.slice(2);
-const noToolbar = args.includes('--no-toolbar') || args.includes('--hide-nav');
-
-console.log('Starting VS Code Web...');
-console.log('');
-console.log('Keybindings:');
-console.log('  Cmd/Ctrl+Q  - Quit');
-console.log('  Cmd/Ctrl+B  - Toggle sidebar');
-console.log('  Cmd/Ctrl+P  - Quick open');
-console.log('');
-
-termweb.open('https://vscode.dev', { toolbar: !noToolbar });
+termweb.open('https://vscode.dev', { toolbar: false });
